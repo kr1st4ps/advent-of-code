e72e31b4094de993ce971fef017e5cad03318d3c
kr1st4ps <kristaps.almanis14@gmail.com>
2023-10-03 20:10:28 +0300

    Completed 2022 Day 6

---
+++ 2022/Day 6/solution_p2.py
@@ -0,0 +1,22 @@
+window = []
+count = 0
+
+with open('input.txt', 'r') as file:
+    while True:
+        char = file.read(1)
+        
+        # Check EOF
+        if not char:
+            break
+        
+        if len(window) == 14:
+            if len(set(window)) == 14:
+                break
+            else:
+                window.pop(0)
+
+        window.append(char)
+
+        count += 1
+        
+print(count)
